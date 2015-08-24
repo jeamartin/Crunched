abb2abccac6a61e69530d1bf83c43d6dbf85e635
jean <onlyjeanmartin@gmail.com>
2015-08-24 17:34:59 +0200

    IGen should now return Stream

--- ExtFile/FileReader.cs
+++ ExtFile/FileReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,15 @@ namespace ExtFile
     [Export(typeof(IGenReader))]
     public class FileReader : IGenReader
     {
+        BinaryReader reader; 
         public string Protocol
         {
             get { return "file"; }
         }
-
         //open
         public void Open(string targetUri) 
         {
-
+            reader = new BinaryReader(File.Open(targetUri, FileMode.Open));
         }
         //read chunk
         public int Read(byte[] buffer, int count)
@@ -27,6 +28,27 @@ namespace ExtFile
             return 0 ; 
         }
         //close
+        public void Dispose()
+        {
+            reader.Close();
+            reader.Dispose();
+        }
+    }
+    [Export(typeof(IGenWriter))]
+    public class FileWriter : IGenWriter
+    {
+        public string Protocol
+        {
+            get { return "file"; }
+        }
+        public void Open(string targetUri)
+        {
+
+        }
+        public int Write(byte[] buffer, int count)
+        {
+            return 0;
+        }
         public void Dispose()
         {
 
